643b9dcacb5a64a1e0e003058f6d0e4a9da55676
dobosz <doboszsite@gmail.com>
2011-04-04 18:16:03 +0200

    Basic Ogre Scene Project. Call me if you need any help

---
+++ Faith-Engine/GameEngine.h
@@ -0,0 +1,106 @@
+#pragma once
+#include <Ogre.h>
+
+using namespace Ogre;
+
+class GameEngine
+{
+protected:
+	Root *mRoot; //G³ówny korzeñ silnika graficznego. Inicjalizacja nastêpuje w funkcji createRoot().
+public:
+	SceneManager *mSceneMgr; //Menadzer sceny, to przez niego umieszczasz elmenty na scenie. Jest public, bo nie wiem jeszcze czy coœ tam nie bêdzie chcia³o sie do nigo odwo³aæ.
+
+	GameEngine(void)
+	{
+	}
+
+	~GameEngine(void)
+	{
+	}
+
+	void go()
+    {
+		//Ta Funkcja jest funkcja która odpala ca³¹ reszte rzeczy potrzebnych do stworzenia sceny Ogra.
+        createRoot();
+        defineResources();
+        setupRenderSystem();
+        createRenderWindow();
+        initializeResourceGroups();
+		setupGUI();
+        setupScene();
+        setupInputSystem();
+        createFrameListener();
+        startRenderLoop();
+    }
+
+	 void createRoot()
+    {
+		mRoot = new Root(); //Tworzenie rdzenia, korzenia silnika graficznego.
+    }
+
+	 void defineResources()
+    {
+		//Napisa³em to funkcje ju¿ downo temu (chyba ja, ale nie jestem pewien na 100% :D). Odczytuje ona z pliku resources.cfg co ma za³adowaæ silnik. O szczegu³y wypataj mnie.
+		String secName, typeName, archName;
+        ConfigFile cf;
+        cf.load("resources.cfg");
+		ConfigFile::SectionIterator seci = cf.getSectionIterator();
+        while (seci.hasMoreElements())
+        {
+			secName = seci.peekNextKey();
+            ConfigFile::SettingsMultiMap *settings = seci.getNext();
+            ConfigFile::SettingsMultiMap::iterator i;
+			for (i = settings->begin(); i != settings->end(); ++i)
+            {
+                typeName = i->first;
+                archName = i->second;
+                ResourceGroupManager::getSingleton().addResourceLocation(archName, typeName, secName);
+            }
+        }
+    }
+
+	 void setupRenderSystem()
+    {
+		//Dobra to coœ pozwala ci na wybór czy renderujesz obraz w DirectX'ie czy w OpenGL'u. Rozdzielczoœæ i takie tam.
+		//Jak plik Ogre.cfg istnieje i mo¿na go odczytaæ to odpala konfigi z niego jak nie to opdala dialogboxa z opcjami konfiguracji.
+		if (!mRoot->restoreConfig() && !mRoot->showConfigDialog())
+            throw Exception(52, "User canceled the config dialog!", "Application::setupRenderSystem()");//Jak waszystko sie posypie to wywala b³¹d :P.
+    }
+
+	 void createRenderWindow()
+    {
+		//Tworzy okienko w WinApi.
+		 mRoot->initialise(true, "Faith Engine"); // "Faith Engine" - to nazwa okna pod Win32.
+    }
+
+	  void initializeResourceGroups()
+    {
+		TextureManager::getSingleton().setDefaultNumMipmaps(5); //Dotyczy techniki wyœfietlania tekstur. Nie ruszeaj tego. Jak chcesz wiedzieæ wiecej to zapytaj, albo google.
+        ResourceGroupManager::getSingleton().initialiseAllResourceGroups(); //£aduje wszystkie resoursy przeczytane z defineResources().
+    }
+
+	  void setupGUI()
+	{
+		//Na razie nie ma GUI ale bedzie trzeba zrobiæ implementacje jego w przysz³oœci. Czytaj w specyfikacji.
+	}
+
+	  void setupScene()
+    {
+		mSceneMgr = mRoot->createSceneManager(ST_GENERIC, "Default SceneManager"); //Inicjalizacja menadzera sceny.
+	}
+
+	 void setupInputSystem()
+    {
+		//Obs³uga klawiatury i myszki. Bedziemy u¿ywaæ bibliotek OIS, ale to innym razem.
+	}
+
+	 void createFrameListener()
+    {
+		//Tu deklaruje sie Listenery, to takie watki dla g³ównego loopa grafiki. Przyk³aday pokarze ci potem.
+    }
+
+	 void startRenderLoop()
+    {
+		 mRoot->startRendering(); //Odpala g³ówna pêtle renderowania sceny.
+    }
+};
---
+++ Faith-Engine/main.cpp
@@ -0,0 +1,46 @@
+/********************************************************
+*					FAITH ENGINE 2011					*
+*														*
+*					  Marcin Dobosz						*
+*				  Bart³omiej Rinkiewicz					*
+* 		             Micha³ Kwestarz					*
+*														*
+*********************************************************/
+
+//PROJEKT ZAK£ADA, ¯E MASZ OGRA SDK NA C:\Ogre SDK 1.7.1\. JAK JEST INACZEJ TO ZMIEN TO W PROJEKCIE ALBO LEPIEJ ZRÓB TAK JAK JA MAM BO BEDZIESZ ROBI£ SYF (!).
+
+// Ogre main lib
+#include <Ogre.h>
+// adicional window lib (WinAPI), used for ShowMessage function.
+#include <windows.h>
+// Using Ogre class as namespace, linking to other class will be do manualy. Brat do std odnosisz sie np. "std::cout", za to do klasy Ogra odnosimy sie bespoœrednio.
+#include <GameEngine.h>
+// Nasza klasa samej gry. Nie zawiera mechaniki, ale loopa renderowania.
+using namespace Ogre;
+
+//Kod zak³ada, ¿e jedziemy na WIN32. Podstawowa obs³uga b³êdów i takie tam.
+#if OGRE_PLATFORM == PLATFORM_WIN32 || OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
+#else
+int main(int argc, char **argv) //ta funkcja main wykona siê jak nie jedziemy na WIN32, ale jedziemy.
+#endif
+{
+	//Tworzy klase GameEngine
+    try
+    {
+        GameEngine Engine;
+        Engine.go();
+    }
+    catch(Exception& e) //Prosta obs³uga b³êdów.
+    {
+#if OGRE_PLATFORM == PLATFORM_WIN32 || OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+        MessageBoxA(NULL, e.getFullDescription().c_str(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+        fprintf(stderr, "An exception has occurred: %s\n",
+            e.getFullDescription().c_str());
+#endif
+    }
+
+    return 0;
+}
