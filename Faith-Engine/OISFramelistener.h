858ad4e61ab9cbd2468742b405dffdc7c53882d1
dobosz <doboszsite@gmail.com>
2011-04-06 14:48:23 +0200

    compilation optimalization

--- Faith-Engine/OISFramelistener.h
+++ Faith-Engine/OISFramelistener.h
@@ -7,77 +7,77 @@ class OISFramelistener : public Ogre::FrameListener, public OIS::KeyListener, pu
 {
 public:
 
-	CEGUIMain* GUI;
-	Ogre::RenderWindow* mWindow;
-	OIS::Keyboard* mKeyboard;
-	OIS::Mouse* mMouse;
-	bool Shutdown;
-	OISFramelistener(Ogre::RenderWindow* Window, OIS::Keyboard* Keyboard, OIS::Mouse* Mouse)
-	{
-		Shutdown = true;
-		mWindow = Window;
-		mKeyboard = Keyboard;
-		mMouse = Mouse;
-		Ogre::WindowEventUtilities::addWindowEventListener(mWindow, this);
-		mKeyboard->setEventCallback(this);
+    CEGUIMain* GUI;
+    Ogre::RenderWindow* mWindow;
+    OIS::Keyboard* mKeyboard;
+    OIS::Mouse* mMouse;
+    bool Shutdown;
+    OISFramelistener(Ogre::RenderWindow* Window, OIS::Keyboard* Keyboard, OIS::Mouse* Mouse)
+    {
+        Shutdown = true;
+        mWindow = Window;
+        mKeyboard = Keyboard;
+        mMouse = Mouse;
+        Ogre::WindowEventUtilities::addWindowEventListener(mWindow, this);
+        mKeyboard->setEventCallback(this);
         mMouse->setEventCallback(this);
-	}
+    }
 
-	~OISFramelistener(void)
-	{
-	}
-	virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt)
-	{
-		mMouse->capture();
-		mKeyboard->capture();
-		if (mWindow->isClosed()) return false;
-		return Shutdown;
-	} 
+    ~OISFramelistener(void)
+    {
+    }
+    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt)
+    {
+        mMouse->capture();
+        mKeyboard->capture();
+        if (mWindow->isClosed()) return false;
+        return Shutdown;
+    } 
 //-------------------------------------------------------------------------------------
-	virtual bool keyPressed( const OIS::KeyEvent &arg )
-	{
+    virtual bool keyPressed( const OIS::KeyEvent &arg )
+    {
 
-		CEGUI::System &sys = CEGUI::System::getSingleton();
-		sys.injectKeyDown(arg.key);
-		sys.injectChar(arg.text);
+        CEGUI::System &sys = CEGUI::System::getSingleton();
+        sys.injectKeyDown(arg.key);
+        sys.injectChar(arg.text);
 
-		switch (arg.key)
-		{
-		case OIS::KC_ESCAPE: 
-			Shutdown = false;
+        switch (arg.key)
+        {
+        case OIS::KC_ESCAPE: 
+            Shutdown = false;
         break;
-		default:
+        default:
         break;
     }
     return true;
-	}
+    }
 //-------------------------------------------------------------------------------------
-	virtual bool keyReleased( const OIS::KeyEvent &arg )
-	{
-		CEGUI::System::getSingleton().injectKeyUp(arg.key);
-		return true;
-	}
+    virtual bool keyReleased( const OIS::KeyEvent &arg )
+    {
+        CEGUI::System::getSingleton().injectKeyUp(arg.key);
+        return true;
+    }
 //-------------------------------------------------------------------------------------
-	virtual bool mouseMoved( const OIS::MouseEvent &arg )
-	{
-		CEGUI::System &sys = CEGUI::System::getSingleton();
-		sys.injectMouseMove(arg.state.X.rel, arg.state.Y.rel);
-		// Scroll wheel.
-		if (arg.state.Z.rel)
-			sys.injectMouseWheelChange(arg.state.Z.rel / 120.0f);
-		return true;
-	}
+    virtual bool mouseMoved( const OIS::MouseEvent &arg )
+    {
+        CEGUI::System &sys = CEGUI::System::getSingleton();
+        sys.injectMouseMove(arg.state.X.rel, arg.state.Y.rel);
+        // Scroll wheel.
+        if (arg.state.Z.rel)
+            sys.injectMouseWheelChange(arg.state.Z.rel / 120.0f);
+        return true;
+    }
 //-------------------------------------------------------------------------------------
-	virtual bool mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id )
-	{
-		CEGUI::System::getSingleton().injectMouseButtonDown(CEGUIMain::convertButton(id));
-		return true;
-	}
+    virtual bool mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id )
+    {
+        CEGUI::System::getSingleton().injectMouseButtonDown(CEGUIMain::convertButton(id));
+        return true;
+    }
 //-------------------------------------------------------------------------------------
-	virtual bool mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id )
-	{
-		CEGUI::System::getSingleton().injectMouseButtonUp(CEGUIMain::convertButton(id));
-		return true;
-	}
+    virtual bool mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id )
+    {
+        CEGUI::System::getSingleton().injectMouseButtonUp(CEGUIMain::convertButton(id));
+        return true;
+    }
 };
 
--- Faith-Engine/OISMain.h
+++ Faith-Engine/OISMain.h
@@ -12,30 +12,30 @@ InputManager* mInputManager;
 Mouse*    mMouse;
 Keyboard* mKeyboard;
 
-	OISMain(void)
-	{
-	}
+    OISMain(void)
+    {
+    }
 
-	~OISMain(void)
-	{
-	}
-	//inicjalizacja OIS.
-	void Initialize(Ogre::RenderWindow* mWindow)
-	{
-		//Wpis do logów.
-		Ogre::LogManager::getSingletonPtr()->logMessage("*** Initializing OIS ***");
-		OIS::ParamList pl;
-		size_t windowHnd = 0;
-		std::ostringstream windowHndStr;
-		mWindow->getCustomAttribute("WINDOW", &windowHnd);
-		windowHndStr << windowHnd;
-		pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
-		mInputManager = OIS::InputManager::createInputSystem( pl );
-		mKeyboard = static_cast<OIS::Keyboard*>(mInputManager->createInputObject(OIS::OISKeyboard, true));
+    ~OISMain(void)
+    {
+    }
+    //inicjalizacja OIS.
+    void Initialize(Ogre::RenderWindow* mWindow)
+    {
+        //Wpis do logów.
+        Ogre::LogManager::getSingletonPtr()->logMessage("*** Initializing OIS ***");
+        OIS::ParamList pl;
+        size_t windowHnd = 0;
+        std::ostringstream windowHndStr;
+        mWindow->getCustomAttribute("WINDOW", &windowHnd);
+        windowHndStr << windowHnd;
+        pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+        mInputManager = OIS::InputManager::createInputSystem( pl );
+        mKeyboard = static_cast<OIS::Keyboard*>(mInputManager->createInputObject(OIS::OISKeyboard, true));
         mMouse = static_cast<OIS::Mouse*>(mInputManager->createInputObject(OIS::OISMouse, true));
-		windowResized(mWindow);
-	}
-	//miejsce do klikania myszk¹.
+        windowResized(mWindow);
+    }
+    //miejsce do klikania myszk¹.
 void windowResized(Ogre::RenderWindow* rw)
 {
     unsigned int width, height, depth;
