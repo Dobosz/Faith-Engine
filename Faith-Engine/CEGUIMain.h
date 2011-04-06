8fec5b4a4c41e43b8e65a5713b5779542a2d9d02
dobosz <doboszsite@gmail.com>
2011-04-06 15:33:49 +0200

    dll and libs

--- Faith-Engine/CEGUIMain.h
+++ Faith-Engine/CEGUIMain.h
@@ -7,26 +7,26 @@ using namespace CEGUI;
 class CEGUIMain
 {
 protected:
-	CEGUI::OgreRenderer* mRenderer;
+    CEGUI::OgreRenderer* mRenderer;
 public:
-	CEGUIMain(void)
-	{
-	}
+    CEGUIMain(void)
+    {
+    }
 
-	~CEGUIMain(void)
-	{
-	}
+    ~CEGUIMain(void)
+    {
+    }
 void initialize()
 {
-	mRenderer = &CEGUI::OgreRenderer::bootstrapSystem();
-	CEGUI::Imageset::setDefaultResourceGroup("Imagesets");
-	CEGUI::Font::setDefaultResourceGroup("Fonts");
-	CEGUI::Scheme::setDefaultResourceGroup("Schemes");
-	CEGUI::WidgetLookManager::setDefaultResourceGroup("LookNFeel");
-	CEGUI::WindowManager::setDefaultResourceGroup("Layouts");
-	CEGUI::SchemeManager::getSingleton().create("WindowsLook.scheme");
-	CEGUI::System::getSingleton().setDefaultMouseCursor("WindowsLook", "MouseArrow");
-	CEGUI::MouseCursor::getSingleton().setImage( CEGUI::System::getSingleton().getDefaultMouseCursor());
+    mRenderer = &CEGUI::OgreRenderer::bootstrapSystem();
+    CEGUI::Imageset::setDefaultResourceGroup("Imagesets");
+    CEGUI::Font::setDefaultResourceGroup("Fonts");
+    CEGUI::Scheme::setDefaultResourceGroup("Schemes");
+    CEGUI::WidgetLookManager::setDefaultResourceGroup("LookNFeel");
+    CEGUI::WindowManager::setDefaultResourceGroup("Layouts");
+    CEGUI::SchemeManager::getSingleton().create("WindowsLook.scheme");
+    CEGUI::System::getSingleton().setDefaultMouseCursor("WindowsLook", "MouseArrow");
+    CEGUI::MouseCursor::getSingleton().setImage( CEGUI::System::getSingleton().getDefaultMouseCursor());
 }
 static CEGUI::MouseButton convertButton(OIS::MouseButtonID buttonID)
 {
@@ -34,13 +34,13 @@ static CEGUI::MouseButton convertButton(OIS::MouseButtonID buttonID)
     {
     case OIS::MB_Left:
         return CEGUI::LeftButton;
- 
+
     case OIS::MB_Right:
         return CEGUI::RightButton;
- 
+
     case OIS::MB_Middle:
         return CEGUI::MiddleButton;
- 
+
     default:
         return CEGUI::LeftButton;
     }
